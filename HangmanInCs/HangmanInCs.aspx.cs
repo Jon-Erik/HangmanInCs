c3c68227257a93f6ec7ef7d21889b62a0a6806ab
Jon-Erik <jdc151@case.edu>
2018-08-22 15:58:41 -0400

    good progress on game logic and gameplay

--- HangmanInCs/HangmanInCs.aspx.cs
+++ HangmanInCs/HangmanInCs.aspx.cs
@@ -20,7 +20,7 @@ namespace HangmanInCs
         {
             if (!IsPostBack)
             {
-                messagelabel.Text = "Press any key to begin";
+                messagelabel.Text = "Press Start/reset button to begin";
                 gameplaylabel.Text = "Let's get started!";
             }           
         }
@@ -28,87 +28,115 @@ namespace HangmanInCs
         private string[] computerOptions = { "VARIABLE", "PROGRAMMING", "APPLICATION", "ASSEMBLY", "INTELLISENSE",
             "CSHARP", "DOCUMENT", "EXCEPTION", "CAMELCASE", "DATABASE", "SCHEMA", "SYSTEM" };
 
-        private int wins = 0;
+        private static int wins = 0;
 
-        private string userGuess = "A";
+        private static int remainingGuesses = 10;
 
-        private int remainingGuesses = 10;
+        public static bool gameStarted = false;
 
-        public bool gameStarted = true;
+        public static bool completedWord = false;
 
-        private bool winContent = false;
+        private static List<Letter> currentWord = new List<Letter>();
 
-        private List<Letter> currentWord = new List<Letter>();
-
-        private string lettersGuessed = "";
+        private static List<Letter> lettersGuessed = new List<Letter>();
 
         private string computerChoice = "test";
 
-        private int remainingLetters = 0;
+        private static int remainingLetters = 0;
 
-        private string alreadyGuessed = "You already guessed that letter.";
-
-        private string winMessage = "You win! Press a button to play again.";
+        private string winMessage = "<p>The word is:<p>" +
+                                    "<p>" + PrintCurrentWord() + "<p>" +
+                                    "<p>Total wins: " + wins.ToString() + "<p>" +
+                                    "<p>Press a button to play again<p>";
 
         private void DisplayGamePlayInfo ()
         {
             gameplaylabel.Text = "<p>Wins: " + wins + "</p>" +
                 "<p>Current Word: " + PrintCurrentWord() + "</p>" +
                 "<p>Remaining Guesses: " + remainingGuesses + "</p>" +
-                "<p>Letters Already Guessed: " + lettersGuessed + "</p>";
+                "<p>Letters Already Guessed: " + PrintGuessedLetters() + "</p>";
         }
 
         private void GetRandomWord()
         {
             Random randomNum = new Random();
             int randomIndex = randomNum.Next(computerOptions.Length);
-            computerChoice = computerOptions[randomIndex];
-            
+            computerChoice = computerOptions[randomIndex];            
         }
 
         protected void Button_Click(object sender, EventArgs e)
-        {
-            string displayString = "";
-            foreach (Letter letter in currentWord)
-            {
-                displayString += letter.Character;
-            }
-            TestLabel.Text = displayString;
+        {           
 
             if (gameStarted == true)
             {
                 System.Web.UI.WebControls.Button button = sender as System.Web.UI.WebControls.Button;
                 string userGuess = button.ID;
-                //TestLabel.Text = userGuess;
-                /*string displayString = "";
 
-                foreach (Letter letter in currentWord)
+                char guessedLetter = userGuess[0];
+                bool correctGuess = false;
+                bool alreadyGuessed = false;
+
+                for (int i = 0; i < lettersGuessed.Count; i++)
                 {
-                    displayString += letter.Character;
-                }
-
-                foreach (Letter letter in currentWord)
-                {
-
-                    if (letter.Character.ToString() == userGuess)
+                    if (lettersGuessed[i].Character == guessedLetter)
                     {
-                        letter.Guessed = true;
-                        displayString += letter.Character.ToString();
-                    }                   
-                }
-                TestLabel.Text = displayString;  */            
-            }
-             
-            DisplayGamePlayInfo();           
+                        alreadyGuessed = true;
+                        messagelabel.Text = "You already guessed that letter.";
+                    }
+                }                
+
+                if (alreadyGuessed == false)
+                {
+                    foreach (Letter letter in currentWord)
+                    {
+                        if (letter.Character == guessedLetter)
+                        {
+                            letter.Guessed = true;
+                            correctGuess = true;
+                            messagelabel.Text = "Correct Guess!";
+                            remainingLetters--;
+
+                            if (remainingLetters == 0)
+                            {
+                                wins++;
+                                letter.Guessed = true;
+                                gameStarted = false;
+                                messagelabel.Text = "You win! Press a button to play again!";
+                                completedWord = true;
+                            }
+                        }
+                    }
+
+                    if (correctGuess == false)
+                    {
+                        messagelabel.Text = "Sorry, incorrect.";
+                        remainingGuesses--;
+                    }
+
+                    lettersGuessed.Add(new Letter { Character = guessedLetter, Guessed = false });
+                    DisplayGamePlayInfo();
+
+                    if (completedWord == true)
+                    {
+                        gameplaylabel.Text = winMessage;
+                    }
+                } 
+            }            
         }
 
         protected void StartResetButton_Click(object sender, EventArgs e)
         {
+            currentWord.Clear();
+            lettersGuessed.Clear();
+            wins = 0;
+            remainingGuesses = 10;
+
             GetRandomWord();
             CreateCurrentWordList();
             remainingLetters = computerChoice.Length;
             DisplayGamePlayInfo();
             ToggleGameStarted();
+            messagelabel.Text = "To start playing, click a letter to make a guess!";
         }
 
         protected void ToggleGameStarted()
@@ -122,22 +150,18 @@ namespace HangmanInCs
             }
         }
 
-        private void CreateCurrentWordList()
+        protected void CreateCurrentWordList()
         {
             char[] letterArray = computerChoice.ToCharArray();
             for (int i = 0; i < letterArray.Length; i++)
             {
                 currentWord.Add(new Letter { Character = letterArray[i], Guessed = false });
             }
-            /*string displayString = "";
-            foreach (Letter letter in currentWord)
-            {
-                displayString += letter.Character;
-            }
-            TestLabel.Text = displayString;*/
+            string displayString = PrintCurrentWord();
+            TestLabel.Text = displayString;
         }
 
-        private string PrintCurrentWord()
+        protected static string PrintCurrentWord()
         {
             string printedString = "";
             foreach (Letter letter in currentWord)
@@ -152,6 +176,22 @@ namespace HangmanInCs
                 }
             }
             return printedString;
+        }
+
+        protected string PrintGuessedLetters()
+        {
+            string printedString = "";
+            foreach (Letter letter in lettersGuessed)
+            {
+                printedString += letter.Character.ToString().ToLower();
+            }
+            return printedString;
+        }
+
+        protected void testbutton_Click(object sender, EventArgs e)
+        {
+            string displayString = PrintCurrentWord();
+            TestLabel.Text = displayString;
         }
     }
 }
--- HangmanInCs/HangmanInCs.aspx.designer.cs
+++ HangmanInCs/HangmanInCs.aspx.designer.cs
@@ -283,6 +283,15 @@ namespace HangmanInCs {
         protected global::System.Web.UI.WebControls.Button StartResetButton;
         
         /// <summary>
+        /// testbutton control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button testbutton;
+        
+        /// <summary>
         /// TestLabel control.
         /// </summary>
         /// <remarks>
